4477e8622832d9965b500644e2fe1217b6367125
Romana Akter <urmee04sust@gmail.com>
2022-05-10 14:19:31 -0400

    invoke_function.py file updated by invoking rest of the functions

--- invoke_function.py
+++ invoke_function.py
@@ -18,8 +18,21 @@ print("Invoking function with default arguments")
 introduction_with_default_args()
 
 #TODO: In this file, go ahead and invoke the rest of the functions from the initialize_functions.py file
+print("Invoking function with a mix of default arguments")
+introduction_with_mix_of_default_args("Doe", "John") 
+
+print("Invoking function that return values")
+product_of_two_num(5,6)
+
+print("Invoking function to sum up all the numbers and return the sum")
+add_all_nums(2,10,5,8,5)
+
+print("Invoking lambda function")
+(lambda x: x*2)(12)             
+             
+print("Invoking function equivalent to lambda function")
+double(12)
 
 print("Invoking function if the input string is a palindrome string")
-
-a_palindrome = input("Enter a string: ")
+palindrome = input("Enter a string: ")
 palindrome(a_palindrome)
